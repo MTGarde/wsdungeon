73fb31970d6b7817046ff51d3557eb1f21aaf1bb
MTGarde <s22gardmart@venta.lv>
2026-04-09 16:10:58 +0300

    initial commit

---
+++ src/test/java/com/wsdungeon/dungeon/DungeonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsdungeon.dungeon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DungeonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
